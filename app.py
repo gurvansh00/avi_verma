b0ae3b74e1ce660409f33ca3f7baaa7a3cf8e32b
gurvansh00 <118958451+gurvansh00@users.noreply.github.com>
2023-09-01 22:36:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import numpy as np
 mod1 = load_model('depression.h5')
 mod2 = load_model('anxiety.h5')
 li = [0]*9
-st.set_page_config(layout='wide')
 #taking inputs
 with st.expander("Age(ranges and values)"):
   st.markdown("""
