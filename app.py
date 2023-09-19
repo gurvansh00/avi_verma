79a42c708e2804d6639a2125aebb2341b2d513e7
gurvansh00 <118958451+gurvansh00@users.noreply.github.com>
2023-09-19 11:53:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,19 @@ st.markdown("""Supporting Mental Health in Youth via. AI Learning, or Smhyal, is
 The Smhyal algorithm is based on a modified version of an artificial neural network (ANN), with an accuracy of more than 94% anxiety and depression diagnosis. The model was trained on the data for young adults from the publicly-available Substance Abuse and Mental Health Service Administration (SAMHSA) MH-CLD survey dataset. The algorithm was created by Avi Verma, under the guidance of Dr. Kaustubh Supekar at Stanford University. We hope that Smyhal will positively impact young adults by counteracting the underdiagnosis of anxiety and depression.
 """)
 st.write('None of the data entered in the form or the diagnosis outputs are ever stored or traced.')
-st.sidebar.success('Select the pages')
+with st.sidebar:
+
+  st.markdown(
+        f"""
+            <style>
+                [data-testid="stSidebar"] {{
+                    background-image: url(https://drive.google.com/uc?export=view&id=19bGh52gQKRQt_8G_3k5j1mW8FqUB5Plm);
+                    background-repeat: no-repeat;
+                    padding-top: 10px;
+                    background-position: 10px 10px;
+                }}
+            </style>
+            """,
+        unsafe_allow_html=True,
+  )
 
